7a2ccb47481779b990a62aab8446c76d698ad352
Álvaro Nicolás Climent <akralan1414@gmail.com>
2025-04-14 22:34:58 +0200

    despliegue y conexion bbdd ok

---
+++ src/main/java/com/clinic/ms_citas/controllers/pruebaController.java
@@ -0,0 +1,34 @@
+package com.clinic.ms_citas.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.List;
+
+@RestController
+@RequestMapping("/test")
+public class pruebaController {
+
+    private final DataSource dataSource;
+
+    public pruebaController(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @GetMapping
+    public String prueba() {
+        return "¡La aplicación está funcionando correctamente!";
+    }
+
+    @GetMapping("/test-db-connection")
+    public String testDbConnection() {
+        try (Connection connection = dataSource.getConnection()) {
+            return "¡Conexión a la base de datos exitosa!";
+        } catch (Exception e) {
+            return "Error al conectar con la base de datos: " + e.getMessage();
+        }
+    }
+}
